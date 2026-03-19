cc96bf626e0165534dc2fa83788003059d91e834
Matheus Hermógenes Rios <matheushrios@hotmail.com>
2026-03-19 13:32:01 -0300

    Delete CursosOnline directory

--- CursosOnline/webcursos/app_cursos/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppCursosConfig(AppConfig):
-    name = 'app_cursos'
--- CursosOnline/webcursos/app_cursos/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Cursos(models.Model):
-    title = models.CharField(verbose_name='Título', max_length=200, null=False, blank=False)
-    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False) 
-    description = models.CharField(verbose_name='Descrição', null=False, blank=False)
