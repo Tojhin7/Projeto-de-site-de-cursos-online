5b3cc57ec0a154f36bd9d8a3a0a46a28a29f208c
Tojhin7 <matheushrios@hotmail.com>
2026-03-19 13:29:15 -0300

    Versão atualizada

---
+++ webcursos/app_cursos/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppCursosConfig(AppConfig):
+    name = 'app_cursos'
---
+++ webcursos/webcursos/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from accounts import views
+from app_cursos.views import home, CursosListView, CursosCreateView, CursosUpdateView, CursosDeleteView, logout_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/', views.register_view, name='register'),
+    path('', home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('cursos/', CursosListView.as_view(), name='cursos_list'),
+    path('create/', CursosCreateView.as_view(), name='cursos_create'),
+    path('update/<int:pk>/', CursosUpdateView.as_view(), name='cursos_update'),
+    path('delete/<int:pk>/', CursosDeleteView.as_view(), name='cursos_delete'),
+    path('logout/', logout_view, name='logout'),
+    ]
